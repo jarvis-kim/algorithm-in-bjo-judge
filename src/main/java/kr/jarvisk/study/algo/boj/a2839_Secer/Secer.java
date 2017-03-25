7be70da64ded211df2e467983537a5c99d732eb7
jarvis-kim <jarvis.kim88@gmail.com>
2017-03-25 21:21:34 +0900

    2839 - 설탕 문제 재귀를 이용하여 문제를 풀었을경우 n=1000 만되어도 끝나질 않음. DP로 변경하여 다시 품.

--- src/main/java/kr/jarvisk/study/algo/boj/a2839_Secer/Secer.java
+++ src/main/java/kr/jarvisk/study/algo/boj/a2839_Secer/Secer.java
@@ -31,10 +31,38 @@ public class Secer {
         return Math.min(a, b) + 1;
     }
 
+    public static int solve2(int n) {
+        if ( n == 3 ) {
+            return 1;
+        }
+        if ( n < 5 ) {
+            return -1;
+        }
+
+        int[] d = new int[ n + 1];
+        d[ 1 ] = Integer.MAX_VALUE;
+        d[ 2 ] = Integer.MAX_VALUE;
+        d[ 3 ] = 1;
+        d[ 4 ] = Integer.MAX_VALUE;
+
+        int a, b;
+        for ( int i = 5; i <= n; i++ ) {
+            a = d[ i - 3 ];
+            b = d[ i - 5 ];
+            if ( a == Integer.MAX_VALUE && b == Integer.MAX_VALUE ) {
+                d[ i ] = Integer.MAX_VALUE;
+            } else {
+                d[ i ] = Math.min(a, b) + 1;
+            }
+        }
+
+        return d[ n ] == Integer.MAX_VALUE ? -1 : d[ n ];
+    }
+
     public static void main(String[] args) {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println(n);
+        System.out.println(solve2(n));
     }
 
 }
