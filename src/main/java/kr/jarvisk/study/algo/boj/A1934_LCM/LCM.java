a7a3a4d7f76fc185ce3315c69c1e27f9b2171537
jarvis <jarvis88@daumcorp.com>
2017-05-31 11:47:39 +0900

    1934 - 최소 공배수 https://www.acmicpc.net/problem/1934
    - Main 추가.

--- src/main/java/kr/jarvisk/study/algo/boj/A1934_LCM/LCM.java
+++ src/main/java/kr/jarvisk/study/algo/boj/A1934_LCM/LCM.java
@@ -1,5 +1,8 @@
 package kr.jarvisk.study.algo.boj.A1934_LCM;
 
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 /**
  * 최소 공배수
  *
@@ -17,5 +20,17 @@ public class LCM {
         }
 
         return a * b / bb;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testCase = scanner.nextInt();
+        IntStream.range(0, testCase)
+                .forEach(value -> {
+                    System.out.println(value);
+                    int a = scanner.nextInt();
+                    int b = scanner.nextInt();
+                    System.out.println(solve(a, b));
+                });
     }
 }
