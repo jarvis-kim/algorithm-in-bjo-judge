9e0ddab871cea8aa4d4a14c0c6c70edd68d4c5c1
jarvis <jarvis88@daumcorp.com>
2017-06-18 17:42:17 +0900

    2178 - 미로 탐색 https://www.acmicpc.net/problem/2178
    - bfs로 풀어봤지만 13%정도에서 틀림..

--- src/main/java/kr/jarvisk/study/algo/boj/a2178_Maze/Maze.java
+++ src/main/java/kr/jarvisk/study/algo/boj/a2178_Maze/Maze.java
@@ -1,8 +1,6 @@
 package kr.jarvisk.study.algo.boj.a2178_Maze;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * 미로 탐색
@@ -13,6 +11,54 @@ public class Maze {
 
     private static int VISITED = 2;
     private static int LOAD = 1;
+
+    public static int bfs(int[][] map) {
+        int n = map.length;
+        int m = map[ 0 ].length;
+
+        boolean[][] visited = new boolean[ n ][ m ];
+
+        int count = 1;
+        Queue<Position> queue = new LinkedList<>();
+        queue.add(new Position(0, 0));
+        visited[ 0 ][ 0 ] = true;
+
+        while ( !queue.isEmpty() ) {
+            queue = nextBfs(map, visited, queue, m, n);
+            count++;
+        }
+
+        return count - 1;
+    }
+
+    static Queue<Position> nextBfs(int[][] map, boolean[][] visited, Queue<Position> queue, int m, int n) {
+        Queue<Position> next = new LinkedList<>();
+
+        while ( queue.size() > 0 ) {
+            Position pos = queue.poll();
+            int y = pos.y;
+            int x = pos.x;
+
+            if ( y > 0 && map[ y - 1 ][ x ] == LOAD && !visited[ y - 1 ][ x ]) {
+                next.add(new Position(y - 1, x));
+                visited[ y - 1 ][ x ] = true;
+            }
+            if ( y < n - 1 && map[ y + 1 ][ x ] == LOAD && !visited[ y + 1 ][ x ]) {
+                next.add(new Position(y + 1, x));
+                visited[ y + 1 ][ x ] = true;
+            }
+            if ( x > 0 && map[ y ][ x - 1 ] == LOAD && !visited[ y  ][ x - 1 ]) {
+                next.add(new Position(y, x - 1));
+                visited[ y ][ x - 1 ] = true;
+            }
+            if ( x < m - 1 && map[ y ][ x + 1 ] == LOAD && !visited[ y ][ x + 1]) {
+                next.add(new Position(y, x + 1));
+                visited[ y ][ x + 1 ] = true;
+            }
+        }
+
+        return next;
+    }
 
     public static int solve(int[][] map, int m, int n) {
         List<Integer> list = new ArrayList<>();
@@ -59,6 +105,16 @@ public class Maze {
                 map[ i ][ j ] = Integer.parseInt(s[ j ]);
             }
         }
-        System.out.println(solve(map, m, n));
+        System.out.println(bfs(map));
+    }
+
+    static class Position {
+        public int y;
+        public int x;
+
+        public Position(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
     }
 }
--- src/test/java/kr/jarvisk/study/algo/boj/MazeTest.java
+++ src/test/java/kr/jarvisk/study/algo/boj/MazeTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class MazeTest {
 
     @Test
-    public void test() {
+    public void testDfs() {
         int[][] map = new int[][] {
                 new int[] { 1, 0, 1, 1, 1, 1 },
                 new int[] { 1, 0, 1, 0, 1, 0 },
@@ -19,7 +19,7 @@ public class MazeTest {
     }
 
     @Test
-    public void test2() {
+    public void test2Dfs() {
         int[][] map = new int[][] {
                 new int[] { 1, 1, 0, 1, 1, 0 },
                 new int[] { 1, 1, 0, 1, 1, 0 },
@@ -29,4 +29,65 @@ public class MazeTest {
 
         Assert.assertEquals(9, Maze.solve(map, map[ 0 ].length, map.length));
     }
+
+    @Test
+    public void testBfs() {
+        int[][] map = new int[][] {
+                new int[] { 1, 0, 1, 1, 1, 1 },
+                new int[] { 1, 0, 1, 0, 1, 0 },
+                new int[] { 1, 0, 1, 0, 1, 1 },
+                new int[] { 1, 1, 1, 0, 1, 1 },
+        };
+
+        Assert.assertEquals(15, Maze.bfs(map));
+    }
+
+    @Test
+    public void test2Bfs() {
+        int[][] map = new int[][] {
+                new int[] { 1, 1, 0, 1, 1, 0 },
+                new int[] { 1, 1, 0, 1, 1, 0 },
+                new int[] { 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 0, 1 },
+        };
+
+        Assert.assertEquals(9, Maze.bfs(map));
+    }
+
+    @Test
+    public void test3Bfs() {
+        int[][] map = new int[][] {
+                new int[] { 1 }
+        };
+
+        Assert.assertEquals(1, Maze.bfs(map));
+    }
+
+
+    @Test
+    public void test4Bfs() {
+        int[][] map = new int[][] {
+                new int[] { 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1 },
+        };
+
+        Assert.assertEquals(9, Maze.bfs(map));
+    }
+
+    @Test
+    public void test5Bfs() {
+        int[][] map = new int[][] {
+                new int[] { 1, 1, 1, 1, 1},
+                new int[] { 0, 0, 0, 0, 1},
+                new int[] { 1, 1, 1, 1, 1},
+                new int[] { 1, 0, 1, 0, 0},
+                new int[] { 1, 1, 1, 1, 1}
+        };
+
+        Assert.assertEquals(13, Maze.bfs(map));
+    }
+
+
 }
