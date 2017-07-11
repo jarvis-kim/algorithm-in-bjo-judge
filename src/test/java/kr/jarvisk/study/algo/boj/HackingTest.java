01c41c9765ae6e412cb59ad781f6b8f561b582ee
jarvis-kim <jarvis.kim88@gmail.com>
2017-07-12 01:09:26 +0900

    1325 - 효율적인 해킹 https://www.acmicpc.net/problem/1325
    - 테스트 케이스 추가. (문제에서 사이클이 존재 할수도 있는데, 사이클에 대한 고려는 하지 않았음)

--- src/test/java/kr/jarvisk/study/algo/boj/HackingTest.java
+++ src/test/java/kr/jarvisk/study/algo/boj/HackingTest.java
@@ -1,10 +1,8 @@
 package kr.jarvisk.study.algo.boj;
 
 import kr.jarvisk.study.algo.boj.a1324_Hacking.Hacking;
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -15,13 +13,42 @@ public class HackingTest {
     public void test() {
 
         Hacking.NodeFactory nodeFactory = new Hacking.NodeFactory(5);
-        nodeFactory.pair(3, 1)
+        nodeFactory
+                .pair(3, 1)
                 .pair(3, 2)
                 .pair(4, 3)
                 .pair(5, 3);
         List<Hacking.Node> leaf = nodeFactory.toLeafNodes();
-        assertArrayEquals(new int[]{1, 2}, Hacking.solve(leaf));
+        assertArrayEquals(new int[] { 1, 2 }, Hacking.solve(leaf));
+    }
+
+    @Test
+    public void test2() {
+
+        Hacking.NodeFactory nodeFactory = new Hacking.NodeFactory(5);
+        nodeFactory
+                .pair(3, 1)
+                .pair(3, 2)
+                .pair(4, 3)
+                .pair(5, 3)
+                .pair(5, 2);
+        List<Hacking.Node> leaf = nodeFactory.toLeafNodes();
+        assertArrayEquals(new int[] { 1, 2 }, Hacking.solve(leaf));
     }
 
 
+    /**
+     * 싸이클이 존재하는 경우
+     */
+    @Test
+    public void test3() {
+
+        Hacking.NodeFactory nodeFactory = new Hacking.NodeFactory(5);
+        nodeFactory
+                .pair(3, 2)
+                .pair(2, 1)
+                .pair(1, 3);
+        List<Hacking.Node> leaf = nodeFactory.toLeafNodes();
+        assertArrayEquals(new int[] { 1, 2, 3 }, Hacking.solve(leaf));
+    }
 }
