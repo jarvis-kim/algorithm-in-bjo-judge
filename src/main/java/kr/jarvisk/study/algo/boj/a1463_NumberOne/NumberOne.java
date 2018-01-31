13f1d021fece5fc98e9e34492e4ef813389da551
jarvis-kim <jarvis.kim88@gmail.com>
2018-01-31 21:23:27 +0900

    1463 - 1로만들기   - https://www.acmicpc.net/problem/1463     기존에는 n=1일때를 처리해주지 않아 오답이 나옴.

--- src/main/java/kr/jarvisk/study/algo/boj/a1463_NumberOne/NumberOne.java
+++ src/main/java/kr/jarvisk/study/algo/boj/a1463_NumberOne/NumberOne.java
@@ -7,9 +7,47 @@ package kr.jarvisk.study.algo.boj.a1463_NumberOne;
  */
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class NumberOne {
 
+    /**
+     * 2018. 01. 31일 다시 품.
+     * 기존 solve()는 n=1 일때 처리를 안해줘서 오답이 나옴.
+     * @param n
+     * @return
+     */
+    public static int solve2(int n) {
+        if ( n == 1 ) {
+            return 0;
+        }
+        if ( n <= 3 ) {
+            return 1;
+        }
+        int[] array = new int[ n + 1 ];
+        array[ 1 ] = 0;
+        array[ 2 ] = 1;
+        array[ 3 ] = 1;
+
+        int number3;
+        int number2;
+        int number1;
+        for ( int number = 4; number <= n; number++ ) {
+            number3 = Integer.MAX_VALUE;
+            number2 = Integer.MAX_VALUE;
+            number1 = array[ number - 1 ] + 1;
+            if ( number % 3 == 0 ) {
+                number3 = array[ number / 3 ] + 1;
+            }
+            if ( number % 2 == 0 ) {
+                number2 = array[ number / 2 ] + 1;
+            }
+
+            array[ number ] = IntStream.of(number1, number2, number3).min().getAsInt();
+        }
+
+        return array[ n ];
+    }
 
     public static int solve(int n) {
         if ( n <= 3 ) {
@@ -37,7 +75,7 @@ public class NumberOne {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        System.out.println(solve(x));
+        System.out.println(solve2(x));
     }
 
 }
--- src/test/java/kr/jarvisk/study/algo/boj/NumberOneTest.java
+++ src/test/java/kr/jarvisk/study/algo/boj/NumberOneTest.java
@@ -11,4 +11,14 @@ public class NumberOneTest {
         Assert.assertEquals(1, NumberOne.solve(2));
         Assert.assertEquals(3, NumberOne.solve(10));
     }
+
+    @Test()
+    public void test2() {
+        Assert.assertEquals(0, NumberOne.solve2(1));
+        Assert.assertEquals(1, NumberOne.solve2(2));
+        Assert.assertEquals(3, NumberOne.solve2(10));
+        Assert.assertEquals(4, NumberOne.solve2(30));
+        Assert.assertEquals(5, NumberOne.solve2(25));
+        Assert.assertEquals(3, NumberOne.solve2(8));
+    }
 }
