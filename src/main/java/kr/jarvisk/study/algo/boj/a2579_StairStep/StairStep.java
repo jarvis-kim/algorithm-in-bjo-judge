5ef9a9a61c6ab859766ef8988663d8e277116e0f
jarvis-kim <jarvis.kim88@gmail.com>
2018-02-02 01:51:05 +0900

    2579 - 계단 오르기   - https://www.acmicpc.net/problem/2579     상향식으로 변경.(성공)

--- src/main/java/kr/jarvisk/study/algo/boj/a2579_StairStep/StairStep.java
+++ src/main/java/kr/jarvisk/study/algo/boj/a2579_StairStep/StairStep.java
@@ -5,14 +5,49 @@ import java.util.Scanner;
 /**
  * 계단 오르기
  * https://www.acmicpc.net/problem/2579
- *
- * 시간 초과...
+ * <p>
  */
 public class StairStep {
 
+    /**
+     *
+     * 점화식 : d[ i ] = max(d[ i - 1],d[ i - 2]) + arr[ i ]
+     *
+     * d1은 한칸을 밟는 것
+     * d2는 두칸을 밟는 것
+     *
+     * d2는 한칸에서 왔는듯 두칸에서왔는 연속 세 계단을 밟지 않는다.
+     *
+     * d1는 d1[ seq - 1] 은 무조건 한칸 전에 온것이므로 d2의 한칸전밖에 답이 없다.
+     *
+     * @param stair
+     * @return
+     */
+    public static int solveByDynamic(int[] stair) {
+        int n = stair.length;
+
+        int[] d1 = new int[ n + 1 ];
+        int[] d2 = new int[ n + 1 ];
+
+        d1[ 1 ] = d2[ 1 ] = stair[ 0 ];
+
+        for ( int index = 1, seq = 2; index < stair.length; index++, seq++ ) {
+            d1[ seq ] = d2[ seq - 1 ] + stair[ index ];
+            d2[ seq ] = Math.max(d1[ seq - 2 ], d2[ seq - 2 ]) + stair[ index ];
+        }
+
+        return Math.max(d1[ n ], d2[ n ]);
+    }
+
     private final static int MIN = -9999;
 
-    public static int solve(int[] stair, int idx, int jump) {
+    /**
+     * 시간초과..
+     * 심지어 맞지도 않음..
+     *
+     * 실패의 기록물로 남겨 놓음.
+     */
+    public static int solveByRecursive(int[] stair, int idx, int jump) {
         if ( idx == 0 ) {
             return stair[ idx ];
         }
@@ -22,28 +57,11 @@ public class StairStep {
 
         int jump1 = MIN;
         if ( jump != 1 ) {
-            jump1 = solve(stair, idx - 1, 1);
+            jump1 = solveByRecursive(stair, idx - 1, 1);
         }
-        int jump2 = solve(stair, idx - 2, 2);
+        int jump2 = solveByRecursive(stair, idx - 2, 2);
 
         return Math.max(jump1, jump2) + stair[ idx ];
-
-//        int[] scores1 = new int[ stair.length ];
-//        int[] scores2 = new int[ stair.length ];
-//        int[] scores = new int[ stair.length ];
-//
-//
-//        scores1[ 0 ] = stair[ 0 ];
-//        scores1[ 1 ] = scores1[ 1 ] + stair[ 1 ];
-//
-//        int sc1;
-//        int sc2;
-//        for ( int index = 2; index < stair.length; index++ ) {
-//            sc1 = scores1[ index - 1 ];
-//            scores[ index ] = Math.max(scores[ index - 1 ], scores[ index - 2 ]);
-//        }
-//
-//        return Math.max(scores1[ stair.length - 1 ], scores2[ stair.length - 2 ]);
     }
 
 
@@ -56,7 +74,7 @@ public class StairStep {
             inputs[ index ] = scanner.nextInt();
         }
 
-        System.out.println(solve(inputs, n - 1, 0));
+        System.out.println(solveByDynamic(inputs));
     }
 }
 
--- src/test/java/kr/jarvisk/study/algo/boj/StairStepTest.java
+++ src/test/java/kr/jarvisk/study/algo/boj/StairStepTest.java
@@ -1,20 +1,33 @@
 package kr.jarvisk.study.algo.boj;
 
-        import kr.jarvisk.study.algo.boj.a2579_StairStep.StairStep;
-        import org.junit.Test;
+import kr.jarvisk.study.algo.boj.a2579_StairStep.StairStep;
+import org.junit.Test;
 
-        import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class StairStepTest {
 
+    /**
+     * history of fail
+     */
     @Test
     public void test() {
+        assertEquals(75, StairStep.solveByRecursive(new int[] {10, 20, 15, 25, 10, 20}, 5, 0));
+        assertEquals(96, StairStep.solveByRecursive(new int[] {13, 1, 15, 27, 29, 21, 20}, 6, 0 ));
+        assertEquals(3, StairStep.solveByRecursive(new int[] {1, 1, 0, 0, 1, 1}, 5, 0));
+        assertEquals(4, StairStep.solveByRecursive(new int[] {1, 1, 0, 1, 1}, 4, 0));
+        /**
+         * fail test
+         */
+        assertEquals(116, StairStep.solveByRecursive(new int[] {1, 10, 10, 3, 5, 100}, 5, 0));
+    }
 
-        assertEquals(75, StairStep.solve(new int[] {10, 20, 15, 25, 10, 20}, 5, 0));
-        assertEquals(96, StairStep.solve(new int[] {13, 1, 15, 27, 29, 21, 20}, 6, 0 ));
-        assertEquals(3, StairStep.solve(new int[] {1, 1, 0, 0, 1, 1}, 5, 0));
-        assertEquals(4, StairStep.solve(new int[] {1, 1, 0, 1, 1}, 4, 0));
-        assertEquals(116, StairStep.solve(new int[] {1, 10, 10, 3, 5, 100}, 5, 0));
-
+    @Test
+    public void test2() {
+        assertEquals(75, StairStep.solveByDynamic(new int[] {10, 20, 15, 25, 10, 20}));
+        assertEquals(96, StairStep.solveByDynamic(new int[] {13, 1, 15, 27, 29, 21, 20}));
+        assertEquals(3, StairStep.solveByDynamic(new int[] {1, 1, 0, 0, 1, 1}));
+        assertEquals(4, StairStep.solveByDynamic(new int[] {1, 1, 0, 1, 1}));
+        assertEquals(125, StairStep.solveByDynamic(new int[] {1, 10, 10, 3, 5, 100}));
     }
 }
